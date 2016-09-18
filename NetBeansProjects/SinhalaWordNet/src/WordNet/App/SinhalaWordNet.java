26be6225118ac7c11f3bb9cb902dc747bcc3164a
Hiran <Hiran@DESKTOP-FJ5DNO1>
2016-09-18 10:40:27 +0530

    Clipboard grabber was added

--- NetBeansProjects/SinhalaWordNet/src/WordNet/App/SinhalaWordNet.java
+++ NetBeansProjects/SinhalaWordNet/src/WordNet/App/SinhalaWordNet.java
@@ -10,6 +10,7 @@ import WorNet.Sample.ReadModelSentences;
 import WorNet.Sample.ReadModelWords;
 import WorNet.Sample.WordandSentence;
 import WordNet.UI.MainFrame;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -21,9 +22,8 @@ public class SinhalaWordNet {
 
     /**
      * @param args the command line arguments
-     * @throws java.sql.SQLException
      */
-    public static void main(String[] args) throws SQLException, IOException {
+    public static void main(String[] args) {
 //        new ReadList().read();
 //        new URLReader().readURL();
 //        new ReadModelWords().read();
@@ -33,11 +33,15 @@ public class SinhalaWordNet {
 //        new ConnectWordsAndSentences().selectAllSentences();
 //        new WordandSentence().setDB();
 //        new ConnectWordsAndSentences().modelSearch();
-          MainFrame f;
-          f=new MainFrame();
-        
-          f.go();
-          //f.setVisible(true);
+        MainFrame f;
+        f = new MainFrame();
+        try {
+            f.getFromClipBoard();
+        } catch (UnsupportedFlavorException | IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        f.go();
+        //f.setVisible(true);
     }
-    
+
 }
